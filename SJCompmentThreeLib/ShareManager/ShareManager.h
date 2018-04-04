67cd7fde72377abb79f00ff9c626abc11f1896b4
weibiao li <weibiaoli@weibiaodeiMac.local>
2018-04-04 11:45:41 +0800

    ss

---
+++ SJCompmentThreeLib/ShareManager/ShareManager.h
@@ -0,0 +1,12 @@
+//
+//  ShareManager.h
+//  SJCompmentThreeLib
+//
+//  Created by weibiao li on 2018/4/4.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ShareManager : NSObject
+
+@end
---
+++ SJCompmentThreeLib/ShareManager/ShareManager.m
@@ -0,0 +1,12 @@
+//
+//  ShareManager.m
+//  SJCompmentThreeLib
+//
+//  Created by weibiao li on 2018/4/4.
+//
+
+#import "ShareManager.h"
+
+@implementation ShareManager
+
+@end
